f89244e4d92da742d092f7cdf76895feebf29c8b
Marcos Piñero <marcos.pinero1976@gmail.com>
2026-02-26 10:43:12 -0300

    varias

--- app/Console/Commands/SyncIntegrantes.php
+++ app/Console/Commands/SyncIntegrantes.php
@@ -587,7 +587,6 @@ END as estado, integrante.ds_curriculum as curriculum, integrante.ds_actividades
                             'estado' => $row->estado,
                             'tipo' => $row->tipo,
                             'deddoc' => $row->deddoc,
-                            'investigacion' => $row->investigacion,
                             'institucion' => $row->institucion,
                             'beca' => $row->beca,
                         ];
@@ -601,7 +600,6 @@ END as estado, integrante.ds_curriculum as curriculum, integrante.ds_actividades
                             'estado' => $row->estado,
                             'tipo' => $row->tipo,
                             'deddoc' => $row->deddoc,
-                            'investigacion' => $row->investigacion,
                             'institucion' => $row->institucion,
                             'beca' => $row->beca,
                         ];
