47fe327815e5064aec7938bb2498513f19a0619d
Marcos Piñero <marcos.pinero1976@gmail.com>
2026-03-05 11:25:31 -0300

    varias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use DB;
 
 class AppServiceProvider extends ServiceProvider
