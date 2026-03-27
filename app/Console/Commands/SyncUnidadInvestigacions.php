0337840105a1c114c066da648cc68ce8331fa509
Marcos Piñero <marcos.pinero1976@gmail.com>
2026-03-27 12:34:30 -0300

    varias

--- app/Console/Commands/SyncUnidadInvestigacions.php
+++ app/Console/Commands/SyncUnidadInvestigacions.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SyncUnidadInvestigacions extends Command
 {
